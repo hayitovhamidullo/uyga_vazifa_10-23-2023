16ad121490ef851a6b8ee2c7f4222de2351deb21
Hamidullo <hayitovhamidullo641@gmail.com>
2023-10-23 07:11:04 +0500

    ikkinchi kommitim

--- javacript/script.js
+++ javacript/script.js
@@ -1,15 +1,20 @@
 'use strict';
+
+// claslar 
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+// openModal modal 
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
+
+// closeModal modal 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
