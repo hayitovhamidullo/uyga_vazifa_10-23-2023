655bd49b48c0055afa9ceafb7481d924806269ba
Hamidullo <hayitovhamidullo641@gmail.com>
2023-10-25 07:22:34 +0500

    6 commitim

--- javacript/script.js
+++ javacript/script.js
@@ -1,84 +1,52 @@
-'use strict';
+"use strict";
 
-// claslar 
-const body = document.querySelector('body');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const cookie = document.querySelector(".cookie button");
 
-// openModal modal 
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-// closeModal modal 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-btnsOpenModal.forEach(btnModal =>
-  btnModal.addEventListener('click', openModal)
-);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('.hidden')) closeModal();
+// cookie
+cookie.addEventListener("click", () => {
+  document.querySelector(".cookie").remove();
 });
 
-const cookies = document.createElement('div');
-cookies.classList.add('cookie-message');
-cookies.innerHTML = `
-We use cookied for improved functionality and analytics
+// for Modal
+const ModalOpen = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const ModalCloe = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", ModalOpen));
+btnCloseModal.addEventListener("click", ModalCloe);
+overlay.addEventListener("click", ModalCloe);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    ModalCloe();
+  }
+});
 
-<button class = 'btn btn--close-cookie'>Got it!<button>
-`;
-body.prepend(cookies);
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', () => cookies.remove());
-cookies.style.position = 'fixed';
-cookies.style.bottom = '0';
-cookies.style.background = '#37383d';
+// for tab
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
-cookies.style.height =
-  parseFloat(getComputedStyle(cookies).height, 10) + 30 + 'px';
-
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', () => cookies.remove() );
-
-  // scrol claslari
-  const btnScrollTo = document.querySelector('.btn--scroll-to');
-  const sectionl = document.querySelector('#section--1');
-
-  // scrol funksiyasi 
-  btnScrollTo.addEventListener('click', (e) => {
-    e.preventDefault();
-    sectionl.scrollIntoView();
-    // console.log(btnScrollTo);
-  });
-
-  const colorGenerator = () => {
-    return `rgb(
-      ${Math.trunc(Math.random() * 256 + 1)},
-      ${Math.trunc(Math.random() * 256 + 1)},
-      ${Math.trunc(Math.random() * 256 + 1)}
-    )`;
-  };
-  // console.log(colorGenerator);
-  
-  const navLink = document.querySelector('.nav__links');
-  navLink.addEventListener('click', function (e){
-    e.preventDefault();
-    if(e.target.classList.contains('.nav__links')){
-       console.log(e.target);
-
-       const sectionName = e.target.getAttribute('href');
-       document.querySelector(sectionName).scrollIntoView({behavior: `smooth`});
-    }
-
-  });
+tabsContainer.addEventListener("click", (e) => {
+  let active = e.target.closest(".operations__tab");
+  if(active){
+    var date = active.dataset.tab
+    tabs.forEach((element) => {
+      element.classList.remove("operations__tab--active");
+      active.classList.add("operations__tab--active")
+    });
+    tabsContent.forEach(e=>{e.classList.remove("operations__content--active")})
+    tabsContent[date-1].classList.add("operations__content--active")
+  }
+});
